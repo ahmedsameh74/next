7fc6e2c66bb1eed118d1e3163a9a2cebd8483def
ahmed sameh <asameh1500@gmail.com>
2022-07-24 14:42:35 +0200

    next js

---
+++ pages/about.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+
+const About = () => {
+    return (
+      <>
+        <Head>
+          <title>Ninja List | About</title>
+          <meta name="keywords" content="ninjas" />
+        </Head>
+        <div>
+          <h1>About</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
+            veritatis porro nihil recusandae minus, modi exercitationem quasi
+            explicabo ullam adipisci suscipit quos quibusdam ducimus odit at
+            iure necessitatibus perferendis assumenda.
+          </p>
+        </div>
+      </>
+    );
+}
+ 
+export default About;
